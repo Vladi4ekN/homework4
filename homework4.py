74ecf47d19290e4aa678269d34e0fd84661c1c3d
Vladi4ekN <167472798+Vladi4ekN@users.noreply.github.com>
2024-04-25 15:18:41 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+immutable_var = ([1, 2, 3], 'Vlad', True, 0)
+print(immutable_var)
+immutable_var [0][2] = 4
+print(immutable_var)
+mutable_list = ['Петя', 'Вася', 'Коля']
+print(mutable_list)
+mutable_list [0] = 'Влад'
+mutable_list [1] = 'Георгий'
+mutable_list [2] = 'Виталя'
+print(mutable_list)
